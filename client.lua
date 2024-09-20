8a943c481f0fac6e315bbbfb005f3277798f9173
Kur0den <69957747+Kur0den@users.noreply.github.com>
2024-09-20 20:43:57 +0900

    オプションでalertとnotificationをoffにできるように

--- client.lua
+++ client.lua
@@ -1,24 +1,28 @@
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
-    local alert = lib.alertDialog({
-        header = config.alert.title,
-        content = config.alert.message,
-        centered = true,
-        size = 'xl',
-        labels ={
-            confirm = config.alert.confirmText
-        }
-    })
+    if config.alert.enabled then
+        local alert = lib.alertDialog({
+            header = config.alert.title,
+            content = config.alert.message,
+            centered = true,
+            size = 'xl',
+            labels ={
+                confirm = config.alert.confirmText
+            }
+        })
+        print(alert)
+    end
 
-    print(alert)
 
-    lib.notify({
-        id = 'joinSendDm',
-        title = config.notification.title,
-        description = config.notification.description,
-        type  = 'success',
-        showDuration = true,
-        duration = 10000,
-        position = 'top',
-        icon = config.notification.icon,
-    })
+    if config.notification.enabled then
+        lib.notify({
+            id = 'joinSendDm',
+            title = config.notification.title,
+            description = config.notification.description,
+            type  = 'success',
+            showDuration = true,
+            duration = 10000,
+            position = 'top',
+            icon = config.notification.icon,
+        })
+    end
 end)
--- config.lua
+++ config.lua
@@ -1,11 +1,13 @@
 config = {}
 
 config.alert = {}
+config.alert.enabled = true
 config.alert.title = 'Announce from server' -- タイトルを設定
 config.alert.message = "Welcome to the server!"  -- メッセージを設定
 config.alert.confirmText = 'Confirm' -- ボタンのラベルを設定
 
 config.notification = {}
+config.notification.enabled = true
 config.notification.title = 'Notification title' -- 通知のタイトルを設定
 config.notification.description = 'Notification description' -- 通知の説明を設定
 config.notification.icon = 'face-smile' -- 通知のアイコンを設定
