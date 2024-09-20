d2bf62b037b2650ca63a382afbddff2744e429d2
Kur0den <69957747+Kur0den@users.noreply.github.com>
2024-09-20 09:34:25 +0900

    ox_libの動作環境であるlua5.4を指定

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,8 @@ author 'Kur0den0010'
 description 'Script to send a DM to a character upon completion of character loading'
 version '2.0.0'
 
+lua54 'yes'
+
 shared_script '@ox_lib/init.lua'
 
 client_scripts {
